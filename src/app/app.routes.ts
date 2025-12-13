9120fed0f1a40b0c4467ceec35917e4285ae0bdb
Vicente Deluca <deluca.vicente@gmail.com>
2025-12-12 22:31:19 -0300

    Muestra el formulario, hay que pasarlo a un template y darle estilo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ReservaFormComponent } from './components/reserva-form/reserva-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: ReservaFormComponent },
+];
