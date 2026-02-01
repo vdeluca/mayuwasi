5fa504ab5e0df7722707fc3ade4703a91b31c172
Vicente Deluca <deluca.vicente@gmail.com>
2026-02-01 19:15:51 -0300

    La seleccion de disponibilidad ya está presentable. Falta linkear el reservar a la primer cabaña disponible de la lista para ese tipo. En algun momento, o para otro cliente, se podría hacer un carrusel de cabañas del mismo precio si es que hubiera diferencia

--- src/app/components/tipos-disponibles/tipos-disponibles.component.ts
+++ src/app/components/tipos-disponibles/tipos-disponibles.component.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-tipos-disponibles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './tipos-disponibles.component.html',
   styleUrl: './tipos-disponibles.component.css',
 })
@@ -15,4 +22,6 @@ export class TiposDisponiblesComponent {
 
   @Output()
   seleccionar = new EventEmitter<number>();
+
+  public url_base_api = environment.url_base_api;
 }
