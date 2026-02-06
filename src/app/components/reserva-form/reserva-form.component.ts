b63b4eaf6daea553061deba27219dcb24b26f4a6
Vicente Deluca <deluca.vicente@gmail.com>
2026-02-06 10:24:38 -0300

    Modificaciones para cotizar correctamente por tipo de cabaña en el componente ReservaForm. [TODO] implementar la cotización en el template. Revisar el flujo del evento ngOnInit(). No se ve el botón de submit

--- src/app/components/reserva-form/reserva-form.component.ts
+++ src/app/components/reserva-form/reserva-form.component.ts
@@ -23,7 +23,7 @@ import { Espacio } from '../../interfaces/espacio';
 import { environment } from '../../../environments/environment';
 
 import { CotizacionService } from '../../services/cotizacion.service';
-import { CotizacionReserva } from '../../interfaces/cotizacion';
+import { CotizacionReserva, CotizarReservaRequest } from '../../interfaces/cotizacion';
 import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 
@@ -79,93 +79,97 @@ export class ReservaFormComponent implements OnInit {
   });
 
   ngOnInit(): void {
-//    // Get param from route
-//    const espacioUuid = this.route.snapshot.paramMap.get('espacioUuid')!;
-//    if (!espacioUuid) return;
-//    this.form.valueChanges
-//      .pipe(
-//        debounceTime(300),
-//        distinctUntilChanged()
-//      )
-//      .subscribe(values => {
-//        const { checkin, checkout, pax, espacio } = values;
-//
-//        if (!checkin || !checkout || !pax || !espacio) {
-//          this.cotizacion = undefined;
-//          return;
-//        }
-//
-//        const payload = {
-//          espacio,
-//          pax,
-//          checkin: this.toBackendDateTime(checkin as unknown as Date),
-//          checkout: this.toBackendDateTime(checkin as unknown as Date),
-//        };
-//
-//        this.cotizando = true;
-//
-//        this.cotizacionService.cotizarReserva(payload)
-//          .subscribe({
-//            next: cotizacion => {
-//              this.cotizacion = cotizacion;
-//              this.form.patchValue(
-//                {
-//                  total: cotizacion.total,
-//                },
-//                { emitEvent: false }
-//              );
-//            },
-//            error: (error) => {
-//              this.cotizacion = undefined;
-//              console.log(error);
-//            },
-//            complete: () => {
-//              this.cotizando = false;
-//            }
-//        });
-//      }
-//    );
-//      
-//    this.route.queryParamMap.subscribe(params => {
-//      const checkin = params. get('checkin');
-//      const checkout = params.get('checkout');
-//      const pax = params.get('pax');
-//    
-//      this.form.patchValue({
-//        espacio: espacioUuid,
-//        checkin: checkin ? this.toBackendDateTime(checkin as unknown as Date) : null,
-//        checkout: checkout ? this.toBackendDateTime(checkout as unknown as Date): null,
-//        pax: pax ? Number(pax) : this.form.value.pax,
-//      });
-//    
-//      this.espaciosService.getEspacio(espacioUuid)
-//        .subscribe(espacio => {
-//          this.espacioSeleccionado = espacio;
-//        });
-//    });
-//    
-//    this.route.queryParamMap.subscribe(params => {
-//      // setear el form
-//      this.form.patchValue({ espacio: espacioUuid });
-//  
-//      // buscar el espacio para mostrar su nombre
-//      this.espaciosService.getEspacio(espacioUuid)
-//        .subscribe(espacio => {
-//          this.espacioSeleccionado = espacio;
-//          console.log(this.espacioSeleccionado);
-//        });
-//    });
-//
-//    // Recupera todas las reservas del espacio
-//    this.reservasService
-//      .getReservasByEspacio(espacioUuid)
-//      .subscribe(data => {
-//        this.reservas = data.map(r => ({
-//        checkin: new Date(r.checkin),
-//        checkout: new Date(r.checkout),
-//      }));
-//    });
-//  
+    // Get param from route
+    const espacioUuid = this.route.snapshot.paramMap.get('espacioUuid')!;
+    if (!espacioUuid) return;
+    this.form.valueChanges
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe(values => {
+        const { checkin, checkout, pax, espacio } = values;
+
+        if (!checkin || !checkout || !pax || !espacio) {
+          this.cotizacion = undefined;
+          return;
+        }
+        const checkin_iso = this.toISODate(checkin);
+        const checkout_iso = this.toISODate(checkout);    
+
+        //console.log("Tipo", this.espacioSeleccionado?.tipo_espacio.id);
+        // cast espacio to string
+        const id_str = Number(this.espacioSeleccionado?.tipo_espacio.id)
+        const payload: CotizarReservaRequest = {
+          id: id_str, // asumimos que el backend acepta tipo o espacio representativo
+          checkin: checkin_iso,
+          checkout: checkout_iso,
+          pax: pax
+        };
+        this.cotizando = true;
+
+        this.cotizacionService.cotizarReserva(payload)
+          .subscribe({
+            next: cotizacion => {
+              this.cotizacion = cotizacion;
+              this.form.patchValue(
+                {
+                  total: cotizacion.total,
+                },
+                { emitEvent: false }
+              );
+            },
+            error: (error) => {
+              this.cotizacion = undefined;
+              console.log(error);
+            },
+            complete: () => {
+              this.cotizando = false;
+            }
+        });
+      }
+    );
+      
+    this.route.queryParamMap.subscribe(params => {
+      const checkin = params. get('checkin');
+      const checkout = params.get('checkout');
+      const pax = params.get('pax');
+    
+      this.form.patchValue({
+        espacio: espacioUuid,
+        checkin: checkin ? this.toBackendDateTime(checkin as unknown as Date) : null,
+        checkout: checkout ? this.toBackendDateTime(checkout as unknown as Date): null,
+        pax: pax ? Number(pax) : this.form.value.pax,
+      });
+    
+      this.espaciosService.getEspacio(espacioUuid)
+        .subscribe(espacio => {
+          this.espacioSeleccionado = espacio;
+        });
+    });
+    
+    this.route.queryParamMap.subscribe(params => {
+      // setear el form
+      this.form.patchValue({ espacio: espacioUuid });
+  
+      // buscar el espacio para mostrar su nombre
+      this.espaciosService.getEspacio(espacioUuid)
+        .subscribe(espacio => {
+          this.espacioSeleccionado = espacio;
+          console.log(this.espacioSeleccionado);
+        });
+    });
+
+    // Recupera todas las reservas del espacio
+    this.reservasService
+      .getReservasByEspacio(espacioUuid)
+      .subscribe(data => {
+        this.reservas = data.map(r => ({
+        checkin: new Date(r.checkin),
+        checkout: new Date(r.checkout),
+      }));
+    });
+  
   }
 
   submit = (): void => {
@@ -244,5 +248,18 @@ export class ReservaFormComponent implements OnInit {
     d.setHours(12, 0, 0, 0); // evita corrimientos por timezone
     return d.toISOString(); // 2026-02-10T12:00:00.000Z
   }
+
+  private toISODate(value: Date | string): string {
+    if (!value) {
+      return '';
+    }
+
+    if (value instanceof Date) {
+      return value.toISOString().split('T')[0];
+    }
+
+    return value.includes('T') ? value.split('T')[0] : value;
+  }
+
   
 }
